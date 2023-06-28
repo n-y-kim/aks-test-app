7c45fceed71c6f18746a3144d7a5bb3cde81a776
n-y-kim <nyn2265@gmail.com>
2023-06-28 15:41:35 +0900

    Add opencensus to enable Logging

--- external-api/main.py
+++ external-api/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
+import os
 
-app = FastAPI()
+app = FastAPI()\
+
+#Get Instrumentation Key from environment variable
+ikey = os.environ.get('APPINSIGHTS_INSTRUMENTATION_KEY')
+
+tracer = Tracer(exporter=AzureExporter(connection_string=ikey), sampler=ProbabilitySampler(1.0))
 
 #Write a simple GET method that returns 'Hello World' text
 @app.get("/")
 def root_get():
-    return {"message": "Hello World"}
+    with tracer.span(name="root_get") as span:
+        return {"message": "Hello World"}
